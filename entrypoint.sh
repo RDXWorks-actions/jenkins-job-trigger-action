1b75f2b042f3b5568ada86439e53d46fd9e90f15
Duje Begonja <duje.begonja@rdx.works>
2024-05-22 10:34:33 +0200

    change cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+env | sort
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 & tailscale up --auth-key=$TAILSCALE_AUTHKEY --hostname="github-$(cat /etc/hostname)" --accept-routes
+cat /etc/resolv.conf
+# export ALL_PROXY="socks5://localhost:1055/"
+# export HTTP_PROXY="http://localhost:1055/"
+# export http_proxy="http://localhost:1055/"
 
-tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 & \
-tailscale up --auth-key=$TAILSCALE_AUTHKEY --hostname="github-$(cat /etc/hostname)" --accept-routes
+ALL_PROXY="socks5://localhost:1055/" HTTP_PROXY="http://localhost:1055/" http_proxy="http://localhost:1055/" ruby /app/jenkins_job.rb
 
-export ALL_PROXY="socks5://localhost:1055/"
-export HTTP_PROXY="http://localhost:1055/"
-export http_proxy="http://localhost:1055/"
-
-ruby /app/jenkins_job.rb
